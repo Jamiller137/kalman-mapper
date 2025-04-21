0890787fd5a45ef44394906779cd61b4b316fd0e
Jacob Miller <Jamiller219@gmail.com>
2025-04-21 17:14:56 -0500

    initial similarity function

--- similarity.py
+++ similarity.py
@@ -1,4 +1,5 @@
 import numpy as np
+from zen_mapper import MapperResult
 from zen_mapper.simplex import get_neighborhood, SimplexTree
 
 
@@ -45,3 +46,43 @@ def get_nodes_containing_idx(nodes: list, datapoint_index: int):
 # is redundant -- will eventually remove
 def get_simplex_neighborhood(st: SimplexTree, node_indices):
     return get_neighborhood(st=st, simplex=node_indices)
+
+
+def centroid_similarity(result1: MapperResult, result2: MapperResult, data):
+    central_points1 = get_central_points(nodes=result1.nodes, data=data)
+    central_points2 = get_central_points(nodes=result2.nodes, data=data)
+
+    centroids = set([cp for cp in central_points1])
+    centroids = centroids.union(set([cp for cp in central_points2]))
+
+    total_similarity = 0.0
+    total_valid_centroids = 0
+
+    for centroid_id in centroids:
+        containing_nodes1 = get_nodes_containing_idx(
+            nodes=result1.nodes, datapoint_index=centroid_id
+        )
+        containing_nodes2 = get_nodes_containing_idx(
+            nodes=result2.nodes, datapoint_index=centroid_id
+        )
+        if containing_nodes1 and containing_nodes2:
+            nbhd1 = get_simplex_neighborhood(result1.nerve, containing_nodes1)
+            nbhd2 = get_simplex_neighborhood(result2.nerve, containing_nodes2)
+
+            data_indices1 = set()
+            for node_idx in nbhd1:
+                data_indices1.update(result1.nodes[node_idx])
+
+            data_indices2 = set()
+            for node_idx in nbhd2:
+                data_indices2.update(result2.nodes[node_idx])
+
+            intersection = len(data_indices1.intersection(data_indices2))
+            union = len(data_indices1.union(data_indices2))
+
+            if union > 0:
+                jaccard_sim = intersection / union
+                total_similarity += jaccard_sim
+                total_valid_centroids += 1
+    final_similarity = total_similarity / total_valid_centroids
+    return final_similarity
